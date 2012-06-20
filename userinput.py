60bbd4d6a0e5e333bfc413ede6cbb99b2c5afe9a
Robert <robert@robert-HP-Pavilion-dv7-Notebook-PC.(none)>
2012-06-20 12:18:02 +0000

    Lab Day 2

---
+++ userinput.py
@@ -0,0 +1,11 @@
+#Lab 2 Exercise 5
+# program written by Bob
+# Input- output of user details
+
+firstName= raw_input('Enter your first name: ')
+lastName= raw_input('Enter your last name: ')
+print('Enter your date of birth:')
+month=raw_input('Month?' )
+day=raw_input('Day?' )
+year=raw_input('Year?' )
+print firstName,lastName ,"was born on" , month, day,",",year
